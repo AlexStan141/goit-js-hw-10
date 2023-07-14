7b1f2c2e777dae9c7b434a8ea22de68c69fae9d0
AlexStan141 <61749830+AlexStan141@users.noreply.github.com>
2023-07-14 18:24:46 +0300

    Complete Breed Select

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,26 +1,29 @@
-import axios from 'axios';
-
-axios.defaults.headers.common['x-api-key'] =
+var api_key =
   'live_WX5zlLCZX7W236sBd35JaCOAqdDpb5SedzaV4i3EEWvt4YieCV68YfZsKU798HQZ';
+var breedSelect = document.querySelector('select.breed-select');
 
 function fetchBreeds() {
-  return new Promise((resolve, reject) => {
-    axios
-      .get('https://api.thecatapi.com/v1/breeds')
-      .then(response => resolve(response))
-      .catch(error => reject(error));
-  });
-}
-function fetchCatByBreed(breedId) {
-  console.log(
-    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
-  );
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-      .then(response => resolve(response))
-      .catch(error => reject(error));
-  });
+  const url = 'https://api.thecatapi.com/v1/breeds';
+  fetch(url, {
+    method: 'GET',
+    withCredentials: true,
+    headers: {
+      'X-Auth-Token': api_key,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(resp => resp.json())
+    .then(function (data) {
+      for (let breed of data) {
+        var option = document.createElement('option');
+        option.value = breed.id;
+        option.innerText = breed.name;
+        breedSelect.append(option);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
-export { fetchBreeds, fetchCatByBreed };
+export { fetchBreeds };
--- src/managePage/completeBreedSelect.js
+++ src/managePage/completeBreedSelect.js
@@ -1,5 +1,4 @@
 import { fetchBreeds } from '../cat-api';
-import { completeCatInfo } from './completeCatInfo';
 import { hideComponent, showComponent } from './toggleComponents';
 var breedSelect = document.querySelector('select.breed-select');
 var error = document.querySelector('p.error');
@@ -9,30 +8,7 @@ hideComponent(breedSelect);
 hideComponent(error);
 
 export function completeBreedSelect() {
-  fetchBreeds()
-    .then(response => {
-      if (response.status != 200) {
-        throw new Error(error.innerText);
-      }
-      return response.data;
-    })
-    .then(data => {
-      data.forEach(element => {
-        var breedOption = document.createElement('option');
-        breedOption.value = element.id;
-        breedOption.innerText = element.name;
-        breedOption.addEventListener('click', () => {
-          completeCatInfo(element.id);
-        });
-        breedSelect.append(breedOption);
-      });
-      showComponent(breedSelect);
-      breedSelect.style.top = '0px';
-      breedSelect.style.left = '0px';
-      hideComponent(loader);
-    })
-    .catch(error2 => {
-      showComponent(error);
-      hideComponent(loader);
-    });
+  hideComponent(loader);
+  showComponent(breedSelect);
+  fetchBreeds();
 }
